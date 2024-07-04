81af84caf6e5744e735f7a5ce93e0b05c4fb9411
HashimKhan0 <hashim.sjk@gmail.com>
2024-07-04 17:19:43 -0400

    Update practice.rkt
    we

--- self-reference/practice.rkt
+++ self-reference/practice.rkt
@@ -1,3 +1,7 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname practice) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;;ListofString is one of..
 ;; - empty
 ;; (cons String (cons ListofString))
 ;; interp. a list of strings
